1c7f0f655e51300db30ff485cac59d96b4143dbe
Luna <git@l4.pm>
2020-08-23 16:40:48 -0300

    properly expose library

--- src/main.zig
+++ src/main.zig
@@ -1,1 +1,6 @@
 const sndfile = @import("sndfile.zig");
+pub const Info = sndfile.Info;
+pub const SoundFile = sndfile.SoundFile;
+pub const Mode = sndfile.Mode;
+pub const Format = sndfile.Format;
+pub const Endian = sndfile.Endian;
